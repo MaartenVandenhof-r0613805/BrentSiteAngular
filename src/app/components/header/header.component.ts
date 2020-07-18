16d9555e64d59eb4f73be96492740f4768294f09
Maarten <maartenvdh@live.be>
2020-07-18 19:15:09 +0200

    hamburger button works

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,8 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  clicked = false;
+  display = "block";
+  onClick($event) {
+    console.log("clicked")
+      const links = document.getElementsByClassName("routerLink");
+
+      for (let i = 0; i < links.length; i++) {
+        const link = links[i] as HTMLElement;
+        link.style.display = this.display;
+        console.log("set display to " + this.display)
+      }
+
+      if (this.display == "block" ) this.display = "none";
+      else this.display = "block";
+  }
 
   ngOnInit() {
   }
